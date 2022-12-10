af5bdcf5abcb0c835cc5dfd0573d0f144e703309
iamcco <ooiss@qq.com>
2022-12-10 13:53:03 +0800

    feat(lsp): change resolve_provider to false

--- packages/ds-pinyin-lsp/src/main.rs
+++ packages/ds-pinyin-lsp/src/main.rs
@@ -33,7 +33,7 @@ impl LanguageServer for Backend {
                     TextDocumentSyncKind::INCREMENTAL,
                 )),
                 completion_provider: Some(CompletionOptions {
-                    resolve_provider: Some(true),
+                    resolve_provider: Some(false),
                     trigger_characters: Some(vec![]),
                     work_done_progress_options: Default::default(),
                     all_commit_characters: None,
