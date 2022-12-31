beca1733976f322f1e201ab687d7406b71f041d5
iamcco <ooiss@qq.com>
2022-12-31 18:00:19 +0800

    chore(dicts): add download.sh

---
+++ packages/dict-builder/download.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+curl https://raw.githubusercontent.com/iDvel/rime-ice/main/cn_dicts/8105.dict.yaml -o ./dicts/8105.dict.yaml
+curl https://raw.githubusercontent.com/iDvel/rime-ice/main/cn_dicts/base.dict.yaml -o ./dicts/base.dict.yaml
+curl https://raw.githubusercontent.com/iDvel/rime-ice/main/cn_dicts/ext.dict.yaml -o ./dicts/ext.dict.yaml
+curl https://raw.githubusercontent.com/iDvel/rime-ice/main/cn_dicts/others.dict.yaml -o ./dicts/others.dict.yaml
+curl https://raw.githubusercontent.com/iDvel/rime-ice/main/cn_dicts/sogou.dict.yaml -o ./dicts/sogou.dict.yaml
+curl https://raw.githubusercontent.com/iDvel/rime-ice/main/cn_dicts/tencent.dict.yaml -o ./dicts/tencent.dict.yaml
+curl https://raw.githubusercontent.com/iDvel/rime-ice/main/opencc/others.txt -o ./dicts/others.txt
+curl https://raw.githubusercontent.com/iDvel/rime-ice/main/opencc/emoji.txt -o ./dicts/emoji.txt
